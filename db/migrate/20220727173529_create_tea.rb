d10336991386104c845198c7782985acd9008c6d
Zachary Imbriaco <zachary@imbriaco.com>
2022-07-27 13:39:46 -0400

    Initial Commit.

---
+++ db/migrate/20220727173529_create_tea.rb
@@ -0,0 +1,10 @@
+class CreateTea < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.string :description
+      t.integer :temperature
+      t.integer :brew_time
+    end
+  end
+end
